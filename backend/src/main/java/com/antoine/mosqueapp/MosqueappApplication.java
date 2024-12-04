e2215e1915f05a53864717d0b841dc1406288845
Antoine Ndiaye <antoinendiaye09@gmail.com>
2024-12-04 10:38:17 +0100

    initial commit

---
+++ backend/src/main/java/com/antoine/mosqueapp/MosqueappApplication.java
@@ -0,0 +1,13 @@
+package com.antoine.mosqueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosqueappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosqueappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/antoine/mosqueapp/MosqueappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoine.mosqueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosqueappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
